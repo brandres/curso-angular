6276f6c0daf05a9a7a2d0f9f94abf1b71e48a7b8
Brandon Lara <brandon.lara@hp.com>
2022-09-15 12:21:54 +0200

    calculadora

--- js/caculator.js
+++ js/caculator.js
@@ -1,4 +1,4 @@
-const BUTTONS_LABELS = ['1', '2' ,'3', '+', '4', '5', '6', '-' , '7', '8', '9', '*', '0', '.', 'C', '=']
+const BUTTONS_LABELS = ['1', '2' ,'3', '+', '4', '5', '6', '-' , '7', '8', '9', '*', '0', '.', 'C', '='];
 
 document.addEventListener("DOMContentLoaded", () => {
     BUTTONS_LABELS.forEach(label => {
@@ -7,13 +7,22 @@ document.addEventListener("DOMContentLoaded", () => {
         button.type = 'button';
         button.className = 'btn btn-primary p-0 w-25 mx-auto';
         button.innerText = label;
-        button.onclick = console.log;
+        button.onclick = onCalculatorButtonClick;
         buttonsContainer.appendChild(button);
     });
 });
 
 onCalculatorButtonClick = (pointerEvent) => {
-    if(pointerEvent.target.innterText != '='){
-        
+    let buttonPressed = pointerEvent.target.innerText
+    if( buttonPressed != '='){
+        let calculatorInput = document.getElementById('calculator-input');
+        calculatorInput.value = buttonPressed === 'C' ? '' : calculatorInput.value + buttonPressed;
+    }else{
+        applyCalculation();
     }
+}
+
+applyCalculation = () => {
+    let calculatorInput = document.getElementById('calculator-input');
+        calculatorInput.value = eval(calculatorInput.value);
 }
